fafda826439f1cd73a7fa7bfcc0450668c5d1097
ChiawNa <chiawnak0390@gmail.com>
2024-11-30 21:39:39 +0800

    basic level

--- 2. array-sum.py
+++ 2. array-sum.py
@@ -9,8 +9,11 @@ def simpleArraySum(ar):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    
     ar_count = int(input().strip())
     ar = list(map(int, input().rstrip().split()))
+
     result = simpleArraySum(ar)
     fptr.write(str(result) + '\n')
+
     fptr.close()
